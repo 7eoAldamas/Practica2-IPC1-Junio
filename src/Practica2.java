44b0c9cffed5c0fd86f451a3ae730a6a2e796efa
Leo Aldamas <leo.aldamas@gmail.com>
2021-06-12 17:48:52 -0600

    Primer Commit - Estructura Practica 2

---
+++ src/Practica2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Practica2 {
+//---
+
+    //--- Tamaño Máximo
+    final int TAMANIO_MAXIMO = 30; //Dimensión
+
+    //--- Clientes
+    int[] idClientes;
+    String[] nombres;
+    int[] telefonos;
+    boolean[] prestados;
+    int nuevoID, nuevoNombre, nuevoTel;
+    Scanner sc = new Scanner(System.in);
+
+    //--- Peliculas
+    int[] idPeliculas;
+    String[] nombresPeliculas;
+    String[] categorias;
+    boolean[] disponible;
+    int nuevoIDP, nuevoNombreP, nuevaCate, nuevoDispo;
+
+
+    //--- Constructor Practica2
+    public Practica2(String nombre) {
+        System.out.println(nombre);
+        initPractica();
+    }
+
+    private void initPractica() {
+    }
+
+    public static void main(String[] args) {
+        //--- Código Ejecutable
+
+    }
+}
